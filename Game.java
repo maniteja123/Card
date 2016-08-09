b75b9933740b7b3a5d3a566091ca4917c62249d9
Nandana <mnandana@paypalcorp.com>
2016-08-09 16:22:40 +0530

    Add constants

--- Game.java
+++ Game.java
@@ -5,12 +5,26 @@ import java.util.List;
 
 public abstract class Game {
 	
+	private static int ONE = 1;
+	private static int TWO = 2;
+	private static int THREE = 3;
+	private static int FOUR = 4;
+	private static int FIVE = 5;
+	private static int SIX = 6;
+	private static int SEVEN = 7;
+	private static int EIGHT = 8;
+	private static int NINE = 9;
+	private static int TEN = 10;
+	private static int JACK = 11;
+	private static int QUEEN = 12;
+	private static int KING = 13;
+	
 	private static int numberOfDecks;
 	private static int numberOfPlayers;
 	private static int numberOfCardsInHand;
 	private List<Integer> scores;
 	private List<ArrayList<Card>> hand;
-
+	
 	public abstract void calculate();
 	
 }
